bc2bacca010b75a322684ccd5ec74b13dc3cb1a3
gouxionglai <gouxionglai@163.com>
2020-02-27 17:07:41 +0800

    update login verify

--- redis/src/main/java/com/gxl/study/redis/service/UserService.java
+++ redis/src/main/java/com/gxl/study/redis/service/UserService.java
@@ -42,67 +42,84 @@ public class UserService {
 
     /**
      * 1.是否有erroKey
-     * 2.没有-->登录验证
-     *      2.1.true, ok
-     *      2.2.false, 创建erroKey, 自增1
-     * 3.有-->是否超过登录限制次数
-     *      3.1.没有-->登录验证
-     *          -->true, ok
-     *          -->false, erroKeyValue+1
-     *      3.2.有-->查看剩余失效时间，返回
+     * 2.有-->是否超过次数限制
+     *      2.1-->有，查询剩余时间，返回
+     *      2.2-->无，登录验证
+     *              -->失败, error++
+     *                  -->是否超过次数限制
+     *                      -->是，设置失效时间，3mins
+     *              -->成功，over
+     * 3.无-->登录验证
+     *      3.1-->成功，over
+     *      3.2-->失败，新建key，返回剩余次数
      * @param uname
      * @param upass
      * @return
      */
     public Object checkFilter(String uname, String upass){
-        Map result = new HashMap();
+        Map<String,Object> result = new HashMap<>();
 
-        //1
+        //1 判断是否有erroKey, 区分大小写
         Boolean erroKey = stringRedisTemplate.hasKey("erroKey");
         if(null != erroKey){
-            //2
-            if(!erroKey){
-                boolean b = checkUser(uname, upass);
-                //2.1
-                if(b){
-                   result.put("code",1);
-                   result.put("msg","登录成功");
-                }else{
-                    //2.2
-                    Long erroKey1 = stringRedisTemplate.opsForValue().increment("erroKey");
-                    System.out.println(erroKey1);
-                    result.put("code",0);
-                    result.put("msg","登录失败");
-                    //剩余次数
-                    result.put("expire_times",5-erroKey1);
-                }
-            }else {
-                //3
-
+            //2 有key
+            if(erroKey){
                 String erroKey1 = stringRedisTemplate.opsForValue().get("erroKey");
                 if(erroKey1 != null){
-                    if(Integer.parseInt(erroKey1) >=5){
+                    //2.1
+                    if(Integer.parseInt(erroKey1) >=3){
                         result.put("code",0);
                         result.put("msg","登录失败");
                         result.put("expire_time","还有多少秒后可以重新登录："+stringRedisTemplate.getExpire("erroKey"));
                     }else{
+                        //2.2 登录验证
                         boolean b = checkUser(uname, upass);
-                        //3.1
+                        // 成功
                         if(b){
                             result.put("code",1);
                             result.put("msg","登录成功");
                         }else{
-                            //3.2
+                            //失败
                             Long erroKey2 = stringRedisTemplate.opsForValue().increment("erroKey");
-                            result.put("code",0);
-                            result.put("msg","登录失败");
-                            //剩余次数
-                            result.put("expire_times",5-erroKey2);
+                            //是否超过次数
+                            if(3<=erroKey2){
+                                //设置失效时间 180s
+                                stringRedisTemplate.expire("erroKey",180, TimeUnit.SECONDS);
+                                //返回剩余时间
+                                result.put("code",0);
+                                result.put("msg","登录失败");
+                                //重试时间
+                                result.put("expire_time",180);
+                            }else{
+                                result.put("code",0);
+                                result.put("msg","登录失败");
+                                //剩余次数
+                                result.put("expire_times",3-erroKey2);
+                            }
                         }
                     }
                 }
+            }else {
+                //3 无key
+                boolean b = checkUser(uname, upass);
+                //2.1
+                if(b){
+                    result.put("code",1);
+                    result.put("msg","登录成功");
+                }else{
+                    //2.2
+                    Long erroKey1 = stringRedisTemplate.opsForValue().increment("erroKey");
+                    System.out.println(erroKey1);
+                    try{
+                        result.put("code",0);
+                        result.put("msg","登录失败");
+                        //剩余次数
+                        result.put("expire_times",3-erroKey1);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
             }
-
         }
         return result;
     }
